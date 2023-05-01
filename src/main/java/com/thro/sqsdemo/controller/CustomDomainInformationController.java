a30c8f8ebe6ba7d7aa410eb9ccfc745a0efc2163
whookie <mapper.wookie@gmail.com>
2023-05-01 13:26:43 +0200

    Fixed database inserts and added http error response if insert fails

--- src/main/java/com/thro/sqsdemo/controller/CustomDomainInformationController.java
+++ src/main/java/com/thro/sqsdemo/controller/CustomDomainInformationController.java
@@ -1,6 +1,7 @@
 package com.thro.sqsdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -25,14 +26,18 @@ public class CustomDomainInformationController {
     public void addDomain(@RequestParam String domain) {
         ExtendedDomainInformation domainObject = new ExtendedDomainInformation();
         domainObject.setDomain(domain);
-        database.save(domainObject);
+        try {
+            database.save(domainObject);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Domain already exists");
+        }
     }
 
     @GetMapping(path="/get")
     @ResponseStatus(code = HttpStatus.OK)
     public ResponseEntity<ExtendedDomainInformation> getDomainInformation(@RequestParam String domain) {
-        var result = database.findById(domain);
-        if (result.isPresent())
+        var result = database.findByDomain(domain);
+        if (result.isEmpty())
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Domain not found");
         
         var informationObject = result.get();
--- src/main/java/com/thro/sqsdemo/model/UserRepository.java
+++ src/main/java/com/thro/sqsdemo/model/UserRepository.java
@@ -1,7 +1,9 @@
 package com.thro.sqsdemo.model;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<ExtendedDomainInformation, String> {
-    
+    public Optional<ExtendedDomainInformation> findByDomain(String name);
 }
