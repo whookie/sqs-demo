5978a5fed8eadeb2e20b0be2646b7c294eee63a5
whookie <mapper.wookie@gmail.com>
2023-05-01 18:48:56 +0200

    Added test for SetInformationFields

--- src/test/java/com/thro/sqsdemo/model/ExtendedDomainInformationTest.java
+++ src/test/java/com/thro/sqsdemo/model/ExtendedDomainInformationTest.java
@@ -2,6 +2,8 @@ package com.thro.sqsdemo.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,9 +18,25 @@ class ExtendedDomainInformationTest {
     }
 
     @Test
-    void ExtendedDomainInformation_GetSetInformationFields() {
+    void ExtendedDomainInformation_GetAddInformationFields() {
         var dom = new ExtendedDomainInformation();
         dom.addCustomInformation("Subdomain I", "translate.google.com");
+        var information = dom.getInformationFields();
+        
+        assertEquals(1, information.size());
+        assertEquals("Subdomain I", information.get(0).getName());
+        assertEquals("translate.google.com", information.get(0).getValue());
+    }
+
+    @Test
+    void ExtendedDomainInformation_GetSetInformationFields() {
+        var dom = new ExtendedDomainInformation();
+        var info = new CustomInformationField();
+        info.setName("Subdomain I");
+        info.setValue("translate.google.com");
+        ArrayList<CustomInformationField> informationList = new ArrayList<CustomInformationField>();
+        informationList.add(info);
+        dom.setCustomInformation(informationList);
         var information = dom.getInformationFields();
         
         assertEquals(1, information.size());
