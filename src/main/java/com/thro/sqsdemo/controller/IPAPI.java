f264aa13b23c461744a65e3d2846dedb766c25e1
whookie <mapper.wookie@gmail.com>
2023-05-30 21:47:24 +0200

    Attempted to fix security warning

--- src/main/java/com/thro/sqsdemo/controller/IPAPI.java
+++ src/main/java/com/thro/sqsdemo/controller/IPAPI.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -14,7 +16,8 @@ public class IPAPI {
 
     public static JsonElement getAddressInformation(String address) throws IOException {
         // At this point, assume the address is already validated
-        String ipapi_domain = String.format("https://ipapi.co/%s/json", address);
+        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
+        String ipapi_domain = String.format("https://ipapi.co/%s/json", encodedAddress);
         URLConnection request = new URL(ipapi_domain).openConnection();
         //request.connect();
         BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
